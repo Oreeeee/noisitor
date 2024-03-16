765d7540a1f064192793ff14e259c7205cb49d99
Oreeeee <oree3ee@gmail.com>
2024-03-16 23:51:40 +0100

    Add running the app with Litestar without Vite

--- noisitor-www/app.py
+++ noisitor-www/app.py
@@ -1,20 +1,23 @@
 from litestar import Litestar, get
+from litestar.static_files import create_static_files_router
 import random
-
-
-@get("/")
-async def index() -> str:
-    return "Hello World"
 
 
 @get("/data/unique-ips")
 async def unique_ips() -> str:
-    return random.randint(0, 999)
+    return str(random.randint(0, 999))
 
 
 @get("/data/total-events")
 async def total_events() -> str:
-    return random.randint(0, 999)
+    return str(random.randint(0, 999))
 
 
-app = Litestar([index, unique_ips, total_events])
+app = Litestar(
+    route_handlers=[
+        unique_ips,
+        total_events,
+        create_static_files_router(path="/", directories=["www/dist/"], html_mode=True),
+        create_static_files_router(path="/assets", directories=["www/dist/assets"]),
+    ],
+)

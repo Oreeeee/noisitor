2b4dd280a106a9c53cc975ffc7b6404a66e8f32f
Oreeeee <oree3ee@gmail.com>
2024-03-17 23:30:01 +0100

    Add config and specifying ports

--- noisitord/noisitord.py
+++ noisitord/noisitord.py
@@ -1,7 +1,33 @@
 import scapy.all as sc
+import os
 
 
-def sniffed_event(pkt):
+class NoisitordConfig:
+    interface: str
+    ports: list[int]
+
+
+def load_config() -> None:
+    # Creates config from values in ENV
+    NoisitordConfig.interface = os.environ["NOISITOR_IFACE"]
+    NoisitordConfig.ports = os.environ["NOISITOR_PORTS"].split(",")
+
+
+def create_filter() -> str:
+    # Creates a filter for sc.sniff()
+    args: list[str] = []
+    args.append("inbound")  # This will only list incoming packets
+    args.append("and")
+    # Add ports
+    for i in range(len(NoisitordConfig.ports)):
+        port: int = NoisitordConfig.ports[i]
+        args.append(f"port {port}")
+        args.append("or")
+    args.pop()
+    return " ".join(args)
+
+
+def sniffed_event(pkt) -> None:
     try:
         print(pkt[1].src)
     except (ValueError, AttributeError):
@@ -9,7 +35,8 @@ def sniffed_event(pkt):
 
 
 def main():
-    sc.sniff(iface="enp4s0", filter="inbound", prn=sniffed_event)
+    load_config()
+    sc.sniff(iface=NoisitordConfig.interface, filter=create_filter(), prn=sniffed_event)
 
 
 if __name__ == "__main__":

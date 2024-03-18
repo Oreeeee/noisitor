4955482f60964b823cf584bdfa96575c30b55cb3
Oreeeee <oree3ee@gmail.com>
2024-03-18 21:11:14 +0100

    Port - scapy -> pyshark

--- noisitord/noisitord.py
+++ noisitord/noisitord.py
@@ -1,4 +1,4 @@
-import scapy.all as sc
+import pyshark
 from dataclasses import dataclass
 import os
 import time
@@ -26,32 +26,27 @@ def load_config() -> None:
 def create_filter() -> str:
     # Creates a filter for sc.sniff()
     args: list[str] = []
-    args.append("inbound")  # This will only list incoming packets
+    args.append(
+        "tcp.flags.syn == 1 and tcp.flags.ack == 0"
+    )  # This will only list incoming packets
     args.append("and")
     # Add ports
     for i in range(len(NoisitordConfig.ports)):
         port: int = NoisitordConfig.ports[i]
-        args.append(f"port {port}")
+        args.append(f"tcp.port == {port}")
         args.append("or")
     args.pop()
     return " ".join(args)
 
 
-def sniffed_event(pkt) -> None:
-    try:
-        packet_info: NoisitordEvent = NoisitordEvent(
-            pkt[1].src, pkt[2].dport, ("tcp" if pkt[1].proto == 6 else "udp"), int(time.time())
-        )
-        print(
-            f"SRC: {packet_info.ip}\nPORT: {packet_info.port}\nPROTO: {packet_info.protocol}\nTIME: {packet_info.time}"
-        )
-    except (ValueError, AttributeError) as e:
-        pass
-
-
 def main():
     load_config()
-    sc.sniff(iface=NoisitordConfig.interface, filter=create_filter(), prn=sniffed_event)
+    cap = pyshark.LiveCapture(
+        interface=NoisitordConfig.interface, display_filter=create_filter()
+    )
+    for packet in cap.sniff_continuously():
+        packet_nst = NoisitordEvent(packet.ip.src, packet.tcp.dstport, "tcp", int(time.time()))
+        print(packet_nst.ip, packet_nst.port, packet_nst.protocol, packet_nst.time)
 
 
 if __name__ == "__main__":

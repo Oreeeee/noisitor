8069d290b45ff92bdb964a66fd56103be5f66685
Oreeeee <oree3ee@gmail.com>
2024-03-19 23:19:38 +0100

    Add getting data from the Database in Noisitor WWW

--- noisitor-www/app.py
+++ noisitor-www/app.py
@@ -1,17 +1,26 @@
 from litestar import Litestar, get
 from litestar.static_files import create_static_files_router
-import random
+import motor.motor_asyncio
+import traceback
+import os
 
 
 @get("/data/unique-ips")
 async def unique_ips() -> str:
-    return str(random.randint(0, 999))
+    return str(len(await events_col.distinct("ip")))
 
 
 @get("/data/total-events")
 async def total_events() -> str:
-    return str(random.randint(0, 999))
+    return str(await events_col.count_documents({}))
 
+
+# Connecting to DB stuff
+dbc = motor.motor_asyncio.AsyncIOMotorClient(
+    "db", 27017, username=os.environ["DB_USERNAME"], password=os.environ["DB_PASSWORD"]
+)
+noisitor_db = dbc["noisitor"]
+events_col = noisitor_db["events"]
 
 app = Litestar(
     route_handlers=[
@@ -20,4 +29,5 @@ app = Litestar(
         create_static_files_router(path="/", directories=["www/dist/"], html_mode=True),
         create_static_files_router(path="/assets", directories=["www/dist/assets"]),
     ],
+    exception_handlers={Exception: lambda r, e: traceback.format_exc()},
 )

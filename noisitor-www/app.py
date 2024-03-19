c286a3181f6b272409d8795243574634d4e06bdb
Oreeeee <oree3ee@gmail.com>
2024-03-20 00:30:52 +0100

    Add uptime counter

--- noisitor-www/app.py
+++ noisitor-www/app.py
@@ -1,8 +1,20 @@
 from litestar import Litestar, get
 from litestar.static_files import create_static_files_router
+from threading import Thread
 import motor.motor_asyncio
 import traceback
+import time
 import os
+
+
+class UptimeCounter:
+    uptime: int = 0
+
+    @classmethod
+    def count_uptime(cls) -> None:
+        while True:
+            time.sleep(1)
+            cls.uptime += 1
 
 
 @get("/data/unique-ips")
@@ -15,6 +27,11 @@ async def total_events() -> str:
     return str(await events_col.count_documents({}))
 
 
+@get("/htmx/uptime")
+async def get_uptime() -> str:
+    return "☕ Uptime: " + str(UptimeCounter.uptime)
+
+
 # Connecting to DB stuff
 dbc = motor.motor_asyncio.AsyncIOMotorClient(
     "db", 27017, username=os.environ["DB_USERNAME"], password=os.environ["DB_PASSWORD"]
@@ -22,10 +39,15 @@ dbc = motor.motor_asyncio.AsyncIOMotorClient(
 noisitor_db = dbc["noisitor"]
 events_col = noisitor_db["events"]
 
+# Uptime counter
+uptime: int = 0
+Thread(target=UptimeCounter.count_uptime).start()
+
 app = Litestar(
     route_handlers=[
         unique_ips,
         total_events,
+        get_uptime,
         create_static_files_router(path="/", directories=["www/dist/"], html_mode=True),
         create_static_files_router(path="/assets", directories=["www/dist/assets"]),
     ],

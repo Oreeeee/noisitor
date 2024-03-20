84940efb9af5a304fe8fd4cf8c850db8ad2922ce
Oreeeee <oree3ee@gmail.com>
2024-03-20 21:23:22 +0100

    Add a map

--- noisitor-www/www/main.js
+++ noisitor-www/www/main.js
@@ -1,3 +1,12 @@
 import "./style.css";
 import "@picocss/pico/css/pico.conditional.min.css";
 import "htmx.org";
+import "leaflet"
+import "leaflet/dist/leaflet.css"
+
+var ipMap = L.map("ip-map").setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(ipMap);

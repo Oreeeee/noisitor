6e102b8d4f19360d542684fa439c0f470db53aaa
Oreeeee <oree3ee@gmail.com>
2024-04-02 22:07:54 +0200

    Validate IP addresses in details

--- noisitor-frontend/src/routes/details/[slug]/+page.server.js
+++ noisitor-frontend/src/routes/details/[slug]/+page.server.js
@@ -1,8 +1,24 @@
+function validateIPaddress(ipaddress) {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ipaddress
+    )
+  ) {
+    return true;
+  }
+  return false;
+}
+
 export const load = async ({ params }) => {
-  // TODO: Verify is IP in slug valid, otherwise return error
+  let ip = params.slug;
+  let isValid = false;
+  if (validateIPaddress(ip)) {
+    isValid = true;
+  }
   // TODO: Fetch data here
   return {
     ip: params.slug,
+    isValid: isValid,
     geolocation: {},
     eventList: [],
   };
--- noisitor-frontend/src/routes/details/[slug]/+page.svelte
+++ noisitor-frontend/src/routes/details/[slug]/+page.svelte
@@ -4,9 +4,6 @@
   import IpInfo from "../../../components/IPInfo.svelte";
   import PastEvents from "../../../components/PastEvents.svelte";
 
-  const ip = data.ip;
-  const eventList = data.eventList;
-
   var showingScreen = "ipInfo";
   function setScreen(screen) {
     showingScreen = screen;
@@ -14,25 +11,31 @@
 </script>
 
 <div class="site-contents">
-  <center>
-    <h1>Viewing info about {ip}</h1>
-  </center>
-  <div class="grid pico">
-    <button
-      on:click={() => {
-        setScreen("ipInfo");
-      }}>IP info</button
-    >
-    <button
-      on:click={() => {
-        setScreen("pastEvents");
-      }}>Past events</button
-    >
-  </div>
-  <br />
-  {#if showingScreen == "ipInfo"}
-    <IpInfo ip />
-  {:else if showingScreen == "pastEvents"}
-    <PastEvents eventList />
+  {#if !data.isValid}
+    <center>
+      <h1>{data.ip} is not a valid IP address!</h1>
+    </center>
+  {:else}
+    <center>
+      <h1>Viewing info about {data.ip}</h1>
+    </center>
+    <div class="grid pico">
+      <button
+        on:click={() => {
+          setScreen("ipInfo");
+        }}>IP info</button
+      >
+      <button
+        on:click={() => {
+          setScreen("pastEvents");
+        }}>Past events</button
+      >
+    </div>
+    <br />
+    {#if showingScreen == "ipInfo"}
+      <IpInfo ip />
+    {:else if showingScreen == "pastEvents"}
+      <PastEvents eventList />
+    {/if}
   {/if}
 </div>
